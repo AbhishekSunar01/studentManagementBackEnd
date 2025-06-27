dc6be0d1d24529c653f6061f1481dda1f49228f4
Abhishek Sunar <138812561+AbhishekSunar01@users.noreply.github.com>
2025-06-27 18:44:28 +0545

    first commit

---
+++ src/test/java/com/abhishek/studentmanagementbackend/StudentManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.studentmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
